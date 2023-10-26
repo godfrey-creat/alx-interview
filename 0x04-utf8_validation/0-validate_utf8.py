404a859c6c3af338d0a8a595a48b32b02c90d1b5
godfrey-creat <ogodfrey135@gmail.com>
2023-10-26 10:24:38 +0000

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" UTF-8 validation """
-
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """ 
-    method that determines if a given data set represents a valid utf-8 
     """
+    method that determines if a given data set
+    represents a valid UTF-8 encoding
+    """
+    nbytes = 0
 
-    mbytes = 0
-    
-    k1 = 1 << 7
-    k2 = 1 << 6
-    for j in data:
-        k = 1 << 7
-        if mbytes == 0:
-            while k and j:
-                mbytes += 1
-                k = k >> 1
-              if mbytes == 0:
-                  continue
-              if mbytes == 1 or mbytes > 4:
-                  return False
-              else:
-                  if not (j & k1 and not (j & k2)):
-                      return False
-                mbytes -= 1
-            return mbytes == 0
+    b1 = 1 << 7
+    b2 = 1 << 6
+
+    for i in data:
+        b = 1 << 7
+        if nbytes == 0:
+            while b & i:
+                nbytes += 1
+                b = b >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
+                return False
+        else:
+            if not (i & b1 and not (i & b2)):
+                return False
+        nbytes -= 1
+    return nbytes == 0
