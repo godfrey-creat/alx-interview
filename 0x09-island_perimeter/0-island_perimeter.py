f5cff05783e5f897207085cb20b97183cc1eef51
godfrey-creat <ogodfrey135@gmail.com>
2023-11-28 08:06:08 +0000

    better

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,7 +12,7 @@ def island_perimeter(grid):
     """ function that outputs the perimeter of an island described by grid """
 
     if not grid or not grid[0]:
-        return 0  # Empty grid
+        return 0  
 
     rows, cols = len(grid), len(grid[0])
     perimeter = 0
@@ -20,13 +20,12 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Start with assuming all sides are surrounded by water
+                perimeter += 4
 
-                # Check neighboring cells
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if the upper neighbor is land
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if the left neighbor is land
+                    perimeter -= 2
 
     return perimeter
 
